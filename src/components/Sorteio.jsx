79a30d66dc01e23423faef46c7488dbe2bcb51df
João Miguel Moscardini <joaomiguelmv_04@hotmail.com>
2022-05-05 22:18:29 -0300

    create-redux

--- src/components/Sorteio.jsx
+++ src/components/Sorteio.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import Card from "./Card";
 
 export default props => {
-    const {min, max} = props;
-    const aleratorio = Math.floor(Math.random() * (max - min + 1)) + min;
+
 
     return(
         <Card title='Sorteio dos Números' purple>
             <div>
                 <span>
                     <span>Resultado: </span>
-                    <strong>{ aleratorio }</strong>
+                    <strong>{ 15 }</strong>
                 </span>
             </div>
         </Card>

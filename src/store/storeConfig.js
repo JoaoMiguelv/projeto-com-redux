2b6d3da3f0f0c0b2a997e7e031924415d3cfd703
João Miguel Moscardini <joaomiguelmv_04@hotmail.com>
2022-05-12 22:04:28 -0300

    actions

--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -2,6 +2,25 @@ import { createStore, combineReducers } from "redux";
 
 const reducers = combineReducers({
     numeros: function(state, action) {
+        switch(action.type){
+            case 'NUM_MIN_ALTERADO':
+                return{
+                    ...state,
+                    min: action.payload
+                }
+            case 'NUM_MAX_ALTERADO':
+                return{
+                    ...state,
+                    max: action.payload
+                }
+
+            default:
+                return{
+                    min: 5,
+                    max: 35
+                }
+
+        }
         return {
             min: 5,
             max: 50
